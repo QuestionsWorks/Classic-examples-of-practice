8c720477c735feb9fe4f7314a1ddc0600d286753
lb9726 <1343726739@qq.com>
2017-06-18 22:30:27 +0800

    codes

--- C++高级编程/1.4c++实例/EmployeeDB/Database.cpp
+++ C++高级编程/1.4c++实例/EmployeeDB/Database.cpp
@@ -10,19 +10,21 @@ using namespace std;
 
 namespace Records {
 
-  Database::Database()
-  {
+Database::Database()
+{
     mNextSlot = 0;
     mNextEmployeeNumber = kFirstEmployeeNumber;
-  }
-  Database::~Database()
-  {
-  }
-  Employee& Database::addEmployee(string inFirstName, string inLastName)
-  {
+}
+
+Database::~Database()
+{
+}
+
+Employee& Database::addEmployee(string inFirstName, string inLastName)
+{
     if (mNextSlot >= kMaxEmployees) {
-      cerr << "There is no more room to add the new employee!" << endl;
-      throw exception();
+        cerr << "There is no more room to add the new employee!" << endl;
+        throw exception();
     }
 
     Employee& theEmployee = mEmployees[mNextSlot++];
@@ -32,53 +34,55 @@ namespace Records {
     theEmployee.hire();
 
     return theEmployee;
-  }
-  Employee& Database::getEmployee(int inEmployeeNumber)
-  {
+}
+
+Employee& Database::getEmployee(int inEmployeeNumber)
+{
     for (int i = 0; i < mNextSlot; i++) {
-      if (mEmployees[i].getEmployeeNumber() == inEmployeeNumber) {
-	return mEmployees[i];
-      }
+        if (mEmployees[i].getEmployeeNumber() == inEmployeeNumber) {
+            return mEmployees[i];
+        }
     }
 
     cerr << "No employee with employee number " << inEmployeeNumber << endl;
     throw exception();
-  }
+}
 
-  Employee& Database::getEmployee(string inFirstName, string inLastName)
-  {
+Employee& Database::getEmployee(string inFirstName, string inLastName)
+{
     for (int i = 0; i < mNextSlot; i++) {
-      if (mEmployees[i].getFirstName() == inFirstName &&
-	  mEmployees[i].getLastName() == inLastName) {
-	return mEmployees[i];
-      }
+        if (mEmployees[i].getFirstName() == inFirstName &&
+                mEmployees[i].getLastName() == inLastName) {
+            return mEmployees[i];
+        }
     }
 
     cerr << "No match with name " << inFirstName << " " << inLastName << endl;
     throw exception();
-  }
-  void Database::displayAll()
-  {
-    for (int i = 0; i < mNextSlot; i++) {
-      mEmployees[i].display();
-    }
-  }
+}
 
-  void Database::displayCurrent()
-  {
+void Database::displayAll()
+{
     for (int i = 0; i < mNextSlot; i++) {
-      if (mEmployees[i].getIsHired()) {
-	mEmployees[i].display();
-      }
+        mEmployees[i].display();
     }
-  }
+}
 
-  void Database::displayFormer()
-  {
+void Database::displayCurrent()
+{
     for (int i = 0; i < mNextSlot; i++) {
-      if (!mEmployees[i].getIsHired()) {
-	mEmployees[i].display();
-      }
+        if (mEmployees[i].getIsHired()) {
+            mEmployees[i].display();
+        }
     }
-  }
+}
+
+void Database::displayFormer()
+{
+    for (int i = 0; i < mNextSlot; i++) {
+        if (!mEmployees[i].getIsHired()) {
+            mEmployees[i].display();
+        }
+    }
+}
 }
